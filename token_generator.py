1b95de66cd0d534dedafb2b421106fbd605a40e2
kertser <kertser@yahoo.com>
2023-09-10 21:13:26 +0300

    token generator added

---
+++ token_generator.py
@@ -0,0 +1,28 @@
+import datetime
+import hashlib
+
+
+class TokenGenerator:
+
+    def __init__(self):
+        self.token = None
+        self.date_string = None
+
+    def get_date_string(self):
+        # Get the current date and time
+        self.date_string = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        return self.date_string
+
+    def generate_token_by_date(self, date):
+        # Hash the date string to create a token
+        self.token = hashlib.sha256(date.encode()).hexdigest()
+
+        return self.token
+
+    def get_token(self):
+        # Get the date string and generate a token
+        date = self.get_date_string()
+        token = self.generate_token_by_date(date)
+
+        return token
