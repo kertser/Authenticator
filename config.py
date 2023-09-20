0e2a4eb01e89748802c9a5cfd26ed4c08240d66b
kertser <kertser@yahoo.com>
2023-09-20 17:25:32 +0300

    Adding templates

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
 # Configuration file for the server
 
-#HOST = 'localhost'  # for local test only
-# HOST = '127.0.0.1'  # for local test only
-# HOST = '0.0.0.0'  # will be on the docker, remotely
-HOST = '46.243.233.79' # This is the real IP address of the server (not to be used remotely)
+# HOST = 'localhost'  # for local test only
+HOST = '0.0.0.0'  # will be on the docker, remotely
+# HOST = '46.243.233.79' # This is the real IP address of the server (not to be used remotely)
 PORT = 5000
 
 server_url = f"http://{HOST}:{PORT}"
